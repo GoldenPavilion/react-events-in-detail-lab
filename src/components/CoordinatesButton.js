2dc81352143e906e33f9e6cf6bb31be84a3db51e
GoldenPavilion <rcorystine@gmail.com>
2021-11-10 18:46:08 -0500

    completed

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component{
+    constructor(){
+        super();
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(event){
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+    }
+
+    render(){
+        return (<button onClick={this.clickHandler}>Find Coordinates</button>)
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+    constructor(){
+        super();
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    clickHandler(e){
+        e.persist();
+        setTimeout(() => {this.props.onDelayedClick(e);}, this.props.delay);
+    }
+    
+    render(){
+        return (
+            <div>
+                <button onClick={this.clickHandler}>Delay!</button>
+            </div>
+        )
+    }
+}
